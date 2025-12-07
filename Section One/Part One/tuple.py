37c9535a8d87f65f36ac1d847adaf52350fe3d80
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-07 18:55:46 +0530

    Part One

---
+++ Section One/Part One/tuple.py
@@ -0,0 +1,8 @@
+# A Tuple is a collection which is ordered and unchangeable (immutable).
+# Tuples are written with round brackets ().
+# Main difference between a list and a tuple is that lists are mutable while tuples are immutable.
+# Tuples can contain items of different data types and allow duplicate values.
+
+# Creating a tuple
+data = (1, "Hello", "Max", 3.14, True)
+print(data)  # Output: (1, 'Hello', 'Max', 3.14, True)
