cd5b12e0ef5e2ec34f9acba1543729aded925119
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-22 22:06:16 +0530

    Part One

--- Section One/Part One/dict.py
+++ Section One/Part One/dict.py
@@ -1,1 +1,2 @@
-# A dictionary is a collection datatype which is unordered and mutable. It consists of a collection of key-value pairs
+# A dictionary is a collection datatype which is unordered and mutable, It consists of a collection of key-value pairs
+# So each key value pair maps the key to it's associated value.
