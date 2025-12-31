76d876ea575984a06326984e627c04fb0b04a1cc
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-31 22:44:38 +0530

    Section One

--- Section One/Part One/list.py
+++ Section One/Part One/list.py
@@ -151,5 +151,6 @@ odd_numbers = [x for x in range(1,11) if x % 2 != 0]
 even_numbers = [x for x in range(1,11) if x%2 ==0]
 print("Odd Numbers from 1 to 10:", odd_numbers)  # Output: [1, 3, 5, 7, 9]
 print("Even Numbers from 1 to 10:", even_numbers)  # Output: [2, 4, 6, 8, 10]
+print("----------------------------------------------------")
 
 
