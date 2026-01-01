d0ad8c0ee4433ba47ac02891bd9f3aab632fa81a
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-01-01 23:29:42 +0530

    Section One

--- Section One/Part One/dict.py
+++ Section One/Part One/dict.py
@@ -1,2 +1,13 @@
 # A dictionary is a collection datatype which is unordered and mutable, It consists of a collection of key-value pairs
 # So each key value pair maps the key to it's associated value.
+
+my_dict = {"name": "John", "age": 27, "city": "New York", "gender": "Male"}
+print(my_dict)
+
+#We can also used dict function to create a dictionary
+sample_dict = dict(name="Adam", city="New Jersey", age=28, gender="Male") #Putting keys as arguments
+print(sample_dict)
+print(type(sample_dict)) # Dict
+
+#Accesing Values in a Dictionary
+print("For accesing names: ", my_dict["name"]) #john
