a1051e9cc9e3792666d9404b730f7e2c4d2a5fbb
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-01-16 23:53:43 +0530

    Section One

---
+++ Section One/Part One/set.py
@@ -0,0 +1,3 @@
+# A set is collection data type in Python which is unordered and mutable.
+# But it can only contain unique elements, means it does not allow duplicate values.
+# Created just like a dictionary but we don't use key-value pairs in sets.
