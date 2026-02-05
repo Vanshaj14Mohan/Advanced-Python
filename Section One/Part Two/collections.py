d263123c7465ae9d6f33cd2de6a3a97437fbf21b
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-05 23:59:33 +0530

    Part Two

--- Section One/Part Two/collections.py
+++ Section One/Part Two/collections.py
@@ -14,8 +14,10 @@ from collections import Counter # Need to import Counter from collections module
 test = "Collections"
 counter = Counter(test) # Creating a counter object
 print(counter) # Output: Counter({'o': 2, 'l': 2, 'C': 1, 'e': 1, 'c': 1, 't': 1, 'i': 1, 'n': 1, 's': 1})
+
 test_two = "abcdabdddegh"
 count = Counter(test_two)
 print(count.items()) # Output: dict_items([('a', 2), ('b', 2), ('c', 1), ('d', 4), ('e', 1), ('g', 1), ('h', 1)])
 print(count.most_common(2)) # 2 represent the nu,ber of most common elements we want to see: [('d', 4), ('a', 2)]
 print(count.keys()) # Output: dict_keys(['a', 'b', 'c', 'd', 'e', 'g', 'h'])
+print(count.values()) # Output: dict_values([2, 2, 1, 4, 1, 1, 1])
