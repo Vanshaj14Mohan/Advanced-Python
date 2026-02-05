b9c9c29e03130ded5c2dc9297ea68d3df01832b3
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-05 23:49:46 +0530

    Part Two

--- Section One/Part Two/collections.py
+++ Section One/Part Two/collections.py
@@ -6,3 +6,7 @@
 # 3: OrderedDict
 # 4: defaultdict
 # 5: deque
+
+# Starting with Counter:
+# Counter is basically a subclass of dict used to count the number of occurences of elements in a collection:
+from collections import Counter # Need to import Counter from collections module
