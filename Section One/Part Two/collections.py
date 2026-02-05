00656bcb3ef510e0f33e103865cccf22a5d181be
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-05 23:44:57 +0530

    Part Two

---
+++ Section One/Part Two/collections.py
@@ -0,0 +1,4 @@
+# In this we will learn about collections in python:
+# Collections are basically built-in data structures which are used to store and manipulate data in an efficient way:
+# Five main types of collections module present in python are:
+# 
