a2697feed018891e9c942406f2667efec61dcc7d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-05 23:46:42 +0530

    Part Two

--- Section One/Part Two/collections.py
+++ Section One/Part Two/collections.py
@@ -1,4 +1,8 @@
 # In this we will learn about collections in python:
 # Collections are basically built-in data structures which are used to store and manipulate data in an efficient way:
 # Five main types of collections module present in python are:
-# 
+# 1: Counter
+# 2: namedtuple
+# 3: OrderedDict
+# 4: defaultdict
+# 5: deque
