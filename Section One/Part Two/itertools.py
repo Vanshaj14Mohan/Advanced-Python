f381de58010e4cc4995fd80828232a1d5f777d49
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-13 23:27:49 +0530

    Part Two

--- Section One/Part Two/itertools.py
+++ Section One/Part Two/itertools.py
@@ -1,3 +1,5 @@
 # Itertools module is a collection of tools for handling iterators:
 # They are used to create iterators for efficetive looping.
 # In short iteators are data types that can be used in a for loop, but they don't store their content in memory:
+# Example are list, sets, tuple, strings, dictionaries etc.
+# The itertools offers some advanced tools for handling iterators such as product, permutations, combinations, accumulate, groupby and infinite iterators
