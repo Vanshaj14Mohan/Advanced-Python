48b200a748e5eabb74d2d89729c09de47bf19ca9
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-17 23:59:14 +0530

    Part Two

--- Section One/Part Two/itertools.py
+++ Section One/Part Two/itertools.py
@@ -42,4 +42,8 @@ comb = combinations(combo, 2) # [(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (2, 4),
 # Here 2 represents length of combinations we want to generate:
 print(list(comb))
 
+from itertools import combinations_with_replacement
+comb_wr = [1, 2, 3, 4]
+comb_wr = combinations_with_replacement(comb_wr, 2)
+print(list(comb_wr)) # [(1, 1), (1, 2), (1, 3), (1, 4), (2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (4, 4)]
 
