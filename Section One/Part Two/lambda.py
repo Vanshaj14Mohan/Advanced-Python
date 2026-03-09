8c075687f55c3cb57ee1fe5f015b83575d36690e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-09 22:13:54 +0530

    Part Two

--- Section One/Part Two/lambda.py
+++ Section One/Part Two/lambda.py
@@ -1,3 +1,4 @@
 # In this part we will lean about lambda function and it's usage
 # A lambda function is a small one line anonymous function that is derived that is defined without a name
 # Syntax for writing a lambda expression:
+# lambda arguments: expression
