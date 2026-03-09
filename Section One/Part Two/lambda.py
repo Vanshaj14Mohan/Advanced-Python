aa5436f25b3e27141f46cdd83229caa62c4fa04e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-09 23:37:23 +0530

    Part Two

--- Section One/Part Two/lambda.py
+++ Section One/Part Two/lambda.py
@@ -3,4 +3,13 @@
 # Syntax for writing a lambda expression:
 # lambda arguments: expression
 add_no = lambda x: x+ 20
-print(add_no(10)) # 30
+print(add_no(10)) # 30, pretty much same as a normal function
+
+def addno(x):
+    return x + 10
+
+print(addno(10)) # 20
+
+# Lambda functions can have multiple arguments as well
+mult = lambda x,y: x*y
+print(mult(5, 7)) # 35
