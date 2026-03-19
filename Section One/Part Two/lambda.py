7678757b9006e2f8b57571df4982505b73dd3307
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-19 23:59:24 +0530

    Part Two

--- Section One/Part Two/lambda.py
+++ Section One/Part Two/lambda.py
@@ -58,8 +58,14 @@ demo = [1, 2, 3, 4, 5]
 fil = filter(lambda x: x%2 == 0, demo)
 print(list(fil)) # [2, 4], only even number
 
+#using list omprehension
+d = [ x for x in demo if x%2 == 0]
+print(list(d)) # [2, 4] same output as above
+
+
 demo_two = [1, 2, 3, 4, 5, 6, 7]
 fil_two = filter(lambda x: x%2!=0, demo_two)
 print(list(fil_two)) # [1, 3, 5, 7], only odd numbers
+
 
 
