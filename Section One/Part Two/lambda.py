d06bc08dae7577cdcaa6b5c01e1aec5fff585fbe
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-19 23:47:32 +0530

    Part Two

--- Section One/Part Two/lambda.py
+++ Section One/Part Two/lambda.py
@@ -46,5 +46,9 @@ a = [1, 2, 3, 4, 5]
 b = map(lambda x: x*2, a)
 print(list(b)) # [2, 4, 6, 8, 10]
 
+# We can achieve the same output using list comprehension
+c = [x*2 for x in a]
+print(c) # [2, 4, 6, 8, 10], same output as above
+
 
 
