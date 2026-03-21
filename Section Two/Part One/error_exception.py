f5877a2c603077faab2b699f86ba070873363d51
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-21 23:47:45 +0530

    Part Two

--- Section Two/Part One/error_exception.py
+++ Section Two/Part One/error_exception.py
@@ -1,2 +1,3 @@
 # In this part we'll look into the difference between and error and an exception
 # And what are the most common built-in exceptions and how we raise and handle exceptions
+# And how we can define our own exceptions
