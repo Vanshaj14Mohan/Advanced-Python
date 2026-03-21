fa5d9a0c50d5b8cbfc011e34938aa0b37e34bf8f
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-21 23:49:51 +0530

    Part Two

--- Section Two/Part One/error_exception.py
+++ Section Two/Part One/error_exception.py
@@ -1,3 +1,6 @@
 # In this part we'll look into the difference between and error and an exception
 # And what are the most common built-in exceptions and how we raise and handle exceptions
 # And how we can define our own exceptions
+
+# Syntax error: A syntax error occurs the parser detects a syntactically incorrect statement
+# a = 10 print(a) this would raise an error
