c7764a386a19e45f290240299fdfe2f21407666e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-04-15 16:40:11 +0530

    Section Two

--- Section One/Part Two/lambda.py
+++ Section One/Part Two/lambda.py
@@ -20,7 +20,6 @@ print(mult(5, 7)) # 35
 point2D = [(1, 2), (15, 1), (5, -1), (10, 4)]
 sorted_point = sorted(point2D) # Would sort the point2D from x paramter 
 sorted_ypoint = sorted(point2D, key=lambda x: x[1])
-
 print(point2D)
 print(sorted_point) # [(1, 2), (5, -1), (10, 4), (15, 1)] sorted the x parameter while keeping y paramater unsorted
 print(sorted_ypoint) # [(5, -1), (15, 1), (1, 2), (10, 4)] sorted the y parameter while keeping x paramater unsorted
