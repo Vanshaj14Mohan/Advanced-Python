8eeef96a82232ce0d90491eeff5e96c4f28f21f6
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-04-16 21:42:39 +0530

    Section Two

--- Section One/Part Two/lambda.py
+++ Section One/Part Two/lambda.py
@@ -1,15 +1,15 @@
 # In this part we will lean about lambda function and it's usage
 # A lambda function is a small one line anonymous function that is derived that is defined without a 
-# It's typically used when we need a simple function that is used only once in your code,   
+# It's typically used when we need a simple function that is used only once in your code,
 # or it can be used as an arguments to higher order functions, functions that take functions as arguments
 # They are used along built-in functions sorted map, filter, reduce and so on.
 # Syntax for writing a lambda expression =>  lambda arguments: expression
 
 add_no = lambda x: x+ 20
 print(add_no(10)) # 30, pretty much same as a normal function
+
 def addno(x):
     return x + 10
-
 print(addno(10)) # 20
 
 # Lambda functions can have multiple arguments as well
