163503bd56f4c3a05ceceaceeedb202b59ef7c50
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-04-17 02:03:56 +0530

    Section Two

--- Section Two/Part One/error_exception.py
+++ Section Two/Part One/error_exception.py
@@ -7,10 +7,20 @@
 
 # a = 10 print(a) this would raise an error as there is no new line between two statements
 # And also if we forget to close a parenthesis or a quotation mark it would raise a syntax error as well
-a = 5 + "10"
+#a = 5 + "10"
 #print(a) # will give a type error
 # Some more common build-in exceptions 
 # Module not found error if we write a module that doesn't exists. eg = > import abcd
 # Namer error
 # x = 5
 # y = z, here z is not defined
+
+# File not found error; => If we enter a file then doesn't exist then we get this error
+#f = open("abcd.txt")
+
+# Value error:
+#If a function or operation receives an argument that has the right type but an inappropriate value
+li = [1, 2, 3, 4]
+li.remove(5) # x not in list, value error
+
+# Index error
