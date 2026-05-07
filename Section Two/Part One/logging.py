d739d08fdb2b96447a9acf0f40f998d94defe48e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-05-07 23:27:57 +0530

    Section Two

--- Section Two/Part One/logging.py
+++ Section Two/Part One/logging.py
@@ -1,1 +1,4 @@
 # In this part we'll learn about logging in python and how to use it to log messages in our code
+# Logging is just a way to keep track of events that happen when some software runs.
+# In this part we'll look into different logging levels, different configurations options
+# How to log in different modules, how to use different log handlers, capturing stack traces and using rotating file handlers.
