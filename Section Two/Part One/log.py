c8bafec116ec67ba42339582d44a7d3689dec182
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-06-13 23:49:36 +0530

    Section Two

--- Section Two/Part One/log.py
+++ Section Two/Part One/log.py
@@ -1,4 +1,5 @@
 # Now to log into a different module can create a logger object and use it to log messages.
 import logging
 logger = logging.getLogger(__name__) # __name__ a special variable that holds the name of current module.
+logger.propagate = False
 logger.info("Info from logger object")
