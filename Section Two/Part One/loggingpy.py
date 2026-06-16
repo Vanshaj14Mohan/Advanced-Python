897f0496360bbe189c1b4a78f63e62d2d7599a5e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-06-16 22:56:23 +0530

    Section Two

--- Section Two/Part One/loggingpy.py
+++ Section Two/Part One/loggingpy.py
@@ -13,7 +13,6 @@ logging.warning("A warning message")
 logging.error("An error message")
 logging.critical("A critical message")
 
-
 # Only warning, error and critical will be logged by default as default logging level is warning.
 # Can change the logging level to debug to log all messages.
 
