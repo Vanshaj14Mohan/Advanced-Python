fc339878d3b9756c79357f19354e7fabdd90d350
Vanshaj P Mohan <144845425+Vanshaj14Mohan@users.noreply.github.com>
2026-06-29 23:42:34 +0530

    Files

--- Section Two/Part One/loggingpy.py
+++ Section Two/Part One/loggingpy.py
@@ -2,6 +2,7 @@
 # Logging is just a way to keep track of events that happen when some software runs.
 # In this part we'll look into different logging levels, different configurations options
 # How to log in different modules, how to use different log handlers, capturing stack traces and using rotating file handlers.
+
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 # After importing we can log into five different levels
